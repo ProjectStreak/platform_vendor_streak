0e30c08017a3e01896cf1191da0f8bf17def2ef6
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-04-10 21:23:29 +0200

    vendor: streak-releaser: Integrate ota-gen and cleanup

--- build/tools/release.py
+++ build/tools/release.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 import json
 import os
-import requests
 import sys
 
 try:
@@ -11,60 +10,70 @@ try:
 except ImportError:
     sys.exit("Please install pygithub package via pip")
 
-
 home = str(Path.home())
 token = str(open(home + "/.githubtoken", "r").read().strip())
 g = Github(token)
 
-if len(sys.argv) > 1:
-    if sys.argv[1].find("https") != -1:
-        print("URL detected, downloading...")
-        zip_name = (sys.argv[1]).split("/")[-1]
-        req = requests.get(sys.argv[1], allow_redirects=True)
-        open(zip_name, "wb").write(req.content)
-    elif sys.argv[1].find(".zip") != -1:
-        print("File detected...")
-        zip_name = sys.argv[1]
-    else:
-        sys.exit("Incorrect file/URL!")
-else:
-    sys.exit("Please provide file/URL!")
+ANDROID_BUILD_TOP = os.getenv("ANDROID_BUILD_TOP")
 
-codename = zip_name.split("-")[4]
-date = zip_name.split("-")[5] + "-" + zip_name.split("-")[6]
-date = date.split(".")[0]
-variant = zip_name.split("-")[3]
+try:
+    zip = os.path.abspath(sys.argv[1])
+    zipName = zip.split("/")[-1]
+except IndexError:
+    sys.exit("Incorrect file!")
 
-print("Codename: " + codename)
-print("Date: " + date)
-print("Variant: " + variant)
+if "Streak" not in zip and ".zip" not in zip:
+    sys.exit("Incorrect file!")
 
-devices_json = requests.get("https://raw.githubusercontent.com/ProjectStreak-Devices/official_devices/main/devices.json").text
+codename = zipName.split("-")[4]
+date = (zipName.split("-")[5] + "-" + zipName.split("-")[6]).split(".")[0]
+variant = zipName.split("-")[3]
 
-brands_list = json.loads(devices_json)
-brand = None
-for fbrand in brands_list:
-    for fcodename in brands_list[fbrand]:
-        if fcodename == codename:
-            brand = fbrand
+print("Releasing " + variant + " build for " + codename)
 
-if brand is None:
-    sys.exit("\nERROR: Can't find repo for your device: " + codename)
+repos = g.get_organization("ProjectStreak-Devices").get_repos()
+for r in repos:
+    if codename in r.name and "device_" in r.name:
+        repo = r
+        print("Repo found for your device: " + repo.name)
 
-repo = g.get_repo("ProjectStreak-Devices/device_" + brand + "_" + codename)
-print("Repo found for your device: " + repo.name)
+if not repo:
+    sys.exit("\nERROR: Can't find repo for " + codename) 
 
 tag = date + "-" + variant
 title = "Streak-" + tag
 
-exp = input("Is this an experimental build? (y/n) (Default: y) ")
-if exp == "n":
-    isExperimental = False
-else:
-    isExperimental = True
+try:
+    release = repo.create_git_release(tag, title, "Automated release of " + zipName)
+    print("Uploading asset...")
+    release.upload_asset(zip)
+    print("Asset uploaded!")
+except GithubException:
+    sys.exit("Release already exists!")
 
-release = repo.create_git_release(tag, title, "Automated release of " + zip_name.split("/")[-1], prerelease=isExperimental)
-print("Release created: " + str(release.title))
-print("Uploading asset...")
-release.upload_asset(zip_name)
-print("Asset uploaded!")
+datetime = int(open(ANDROID_BUILD_TOP + "/out/build_date.txt", "r").read())
+url = "https://github.com/ProjectStreak-Devices/" + repo.name + "/releases/download/" + tag + "/" + zipName
+checksum = open(zip + ".sha256sum", "r").read().split(" ")[0]
+filesize = os.path.getsize(zip)
+version = zipName.split("-")[1]
+
+template = {
+  "response": [
+    {
+      "datetime": datetime,
+      "filename": zipName,
+      "id": checksum,
+      "romtype": variant,
+      "size": filesize,
+      "url": url,
+      "version": version
+    }
+  ]
+}
+
+jsonFile = open(ANDROID_BUILD_TOP + "/streak_ota/devices/" + codename + "/" + variant.lower() + ".json", "w")
+jsonFile.write(json.dumps(template, indent=2))
+jsonFile.close()
+
+print("Release created: " + title + "!")
+print("Don't forget to commit and push streak_ota repository!")
