94562864d041b0e57e27dc15943288215890a3ad
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-05-02 15:28:56 +0200

    streak: Move common packages to separate makefile

--- config/common.mk
+++ config/common.mk
@@ -98,81 +98,6 @@ TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \
     bootanimation.zip
 
-# Streak packages
-PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_COPY_FILES += \
-    vendor/streak/prebuilt/common/etc/init/init.streak-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.streak-updater.rc
-
-# Themes
-PRODUCT_PACKAGES += \
-    ThemePicker
-
-# Config
-PRODUCT_PACKAGES += \
-    SimpleDeviceConfig
-
-# Extra tools in Streak
-PRODUCT_PACKAGES += \
-    7z \
-    bash \
-    curl \
-    getcap \
-    htop \
-    lib7z \
-    nano \
-    pigz \
-    setcap \
-    unrar \
-    vim \
-    zip
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/bin/curl \
-    system/bin/getcap \
-    system/bin/setcap
-
-# Openssh
-PRODUCT_PACKAGES += \
-    scp \
-    sftp \
-    ssh \
-    sshd \
-    sshd_config \
-    ssh-keygen \
-    start-ssh
-
-PRODUCT_COPY_FILES += \
-    vendor/streak/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
-
-# rsync
-PRODUCT_PACKAGES += \
-    rsync
-
-# Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.storage_manager.enabled=true
-
-# These packages are excluded from user builds
-PRODUCT_PACKAGES_DEBUG += \
-    procmem
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/bin/procmem
-endif
-
-# Root
-PRODUCT_PACKAGES += \
-    adb_root
-ifneq ($(TARGET_BUILD_VARIANT),user)
-ifeq ($(WITH_SU),true)
-PRODUCT_PACKAGES += \
-    su
-endif
-endif
-
 # Dex preopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
@@ -184,3 +109,6 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 # Inherit from our branding
 $(call inherit-product, vendor/streak/config/branding.mk)
+
+# Inherit from packages
+$(call inherit-product, vendor/streak/config/packages.mk)
---
+++ config/packages.mk
@@ -0,0 +1,74 @@
+# Streak packages
+PRODUCT_PACKAGES += \
+    Updater
+
+PRODUCT_COPY_FILES += \
+    vendor/streak/prebuilt/common/etc/init/init.streak-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.streak-updater.rc
+
+# Themes
+PRODUCT_PACKAGES += \
+    ThemePicker
+
+# Config
+PRODUCT_PACKAGES += \
+    SimpleDeviceConfig
+
+# Extra tools in Streak
+PRODUCT_PACKAGES += \
+    7z \
+    bash \
+    curl \
+    getcap \
+    htop \
+    lib7z \
+    nano \
+    pigz \
+    setcap \
+    unrar \
+    vim \
+    zip
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/curl \
+    system/bin/getcap \
+    system/bin/setcap
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+PRODUCT_COPY_FILES += \
+    vendor/streak/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
+
+# Root
+PRODUCT_PACKAGES += \
+    adb_root
+ifneq ($(TARGET_BUILD_VARIANT),user)
+ifeq ($(WITH_SU),true)
+PRODUCT_PACKAGES += \
+    su
+endif
+endif
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
+
+# Storage manager
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.storage_manager.enabled=true
+
+# These packages are excluded from user builds
+PRODUCT_PACKAGES_DEBUG += \
+    procmem
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/procmem
+endif
