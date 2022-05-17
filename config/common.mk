yaap/vendor_yaap

config: sepolicy: import flipendo/turbo_adapter sepolicy
Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

config/common.mk
@@ -136,3 +136,6 @@ PRODUCT_HOST_PACKAGES += \
 
 # Themes
 $(call inherit-product, vendor/themes/common.mk)
+
+# Sepolicy
+$(call inherit-product, vendor/yaap/config/sepolicy.mk)

ProjectStreak/platform_vendor_streak

config/common.mk
@@ -117,6 +117,9 @@ $(call inherit-product, vendor/streak/config/branding.mk)
 # Inherit from packages
 $(call inherit-product, vendor/streak/config/packages.mk)
 
+# Inherit from sepolicy
+$(call inherit-product, vendor/streak/config/sepolicy.mk)
+
 # Inherit from gapps
 $(call inherit-product, vendor/google/gms/config.mk)
 